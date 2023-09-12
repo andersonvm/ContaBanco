b5893bfab47c0b9974c0d4308212df6acc534228
Anderson Vieira Machado <andersonvm1@gmail.com>
2023-09-12 14:37:21 -0300

    Classe completa

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Por favor informe o número da conta: ");
+		int numeroConta = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Informe o código da agência: ");
+		String codigoAgencia = sc.nextLine();
+		System.out.println("Informe o nome do Cliente: ");
+		String nomeCliente = sc.nextLine();
+		System.out.println("Informe saldo: ");
+		double saldo = sc.nextDouble();
+
+		System.out.println(
+				"Olá, " + nomeCliente.toUpperCase() + " obrigado por criar uma conta em nosso banco, sua agência é "
+						+ codigoAgencia + " conta é " + numeroConta + " e o seu saldo " + saldo
+						+ " já está disponível para saque");
+
+		sc.close();
+	}
 }
